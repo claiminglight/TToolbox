f91f18feb0085a63d60faf2b78efdf40d8e64e61
claiminglight <claiminglight@gmail.com>
2025-06-20 03:03:02 -0500

    First Public Commit

--- CGPTtest.cs
+++ CGPTtest.cs
@@ -1,2 +1,3 @@
 ﻿// This file does nothing, but is meant to provide a proof of concept. Chat, can you see this?
 // Also a test for new content to GitHub, since it was causing me problems.
+// Now we're set to public.
